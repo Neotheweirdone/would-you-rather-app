d6203c493dbce8f6a9aa0bd0c75360621feac557
Neotheweirdone <45073167+Neotheweirdone@users.noreply.github.com>
2020-04-02 21:49:31 +0530

    started with login page

---
+++ src/actions/shared.js
@@ -0,0 +1,10 @@
+import { receiveQuestions } from './questions'
+import { receiveUsers } from './users'
+import { setAuthedUser } from './authedUser'
+import {_getUsers } from '_DATA.js'
+
+export function handleInitialData(){
+    return(dispatch)=>{
+        return _getUsers().then(users)
+    }
+}
---
+++ src/components/Login.js
@@ -0,0 +1,15 @@
+import React,{Component} from 'react'
+
+class Login extends Component{
+    render(){
+        return(<div>
+           <h1 className='center'> Login</h1>
+           <p>Welcome to the Would you rather App!</p>
+           <p>Select an user</p>
+           
+            </div>
+        )
+    }
+}
+
+export default Login
